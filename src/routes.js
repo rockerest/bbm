d20979b9d5cbd066c76dd2eeb32f3a0918a33dd4
rockerest <websites@thomasrandolph.info>
2014-07-11 16:53:58 -0500

    Parent router should wait until I tell it.

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,37 @@
 define(
-    ["require", "backbone", "underscore",
-    "views/navigation",
-    "routers/bbm", "routers/thing"],
+    [
+        // Libraries
+        "require", "backbone", "underscore",
+        // Application-level Views
+        "views/navigation",
+        // Routers
+        "routers/bbm", "routers/thing"
+    ],
     function(
         require, Backbone, _,
         NavigationView,
         BbmRouter, ThingRouter
     ){
-        var routers = [
-                BbmRouter,
-                ThingRouter
-            ],
-            count = 0,
-            nav = new NavigationView(); //Always load the navigation before routing starts
+        var Routes = {};
 
-        _( routers ).each( function( r, i ){
-            ++count;
-            r.register();
+        Routes.startup = function(){
+            var routers = [
+                    BbmRouter,
+                    ThingRouter
+                ],
+                count = 0,
+                nav = new NavigationView(); //Always load the navigation before routing starts
 
-            if( count === routers.length ){
-                Backbone.history.start();
-            }
-        });
+            _( routers ).each( function( r, i ){
+                ++count;
+                r.register();
+
+                if( count === routers.length ){
+                    Backbone.history.start();
+                }
+            });
+        };
+
+        return Routes;
     }
 );
