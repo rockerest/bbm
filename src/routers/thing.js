3423df90889cffcd3323c6d050c40c7f2c0f9b15
rockerest <websites@thomasrandolph.info>
2014-07-12 23:13:58 -0500

    Handle routes for things

--- src/routers/thing.js
+++ src/routers/thing.js
@@ -1,15 +1,16 @@
 define(
-    ["backbone", "layouts/bbm", "views/pages/things"],
-    function( Backbone, BbmLayout, ThingsView ){
+    ["backbone", "layouts/bbm", "views/pages/things", "views/pages/thing"],
+    function( Backbone, BbmLayout, ThingsView, ThingView ){
         var mod = {},
-            ThingRouter = Backbone.Router.extend({
-                routes: {
-                    "things": "things"
-                }
-            });
+            ThingRouter = Backbone.Router.extend();
 
         mod.register = function(){
             var rtr = new ThingRouter;
+
+            rtr.route( "things",                "things"        );
+            rtr.route( /thing\/(\w+)$/,         "do"            );
+            rtr.route( /thing\/(\d+)$/,         "viewThing"     );
+            rtr.route( /thing\/(\w+)\/(\d+)$/,  "modifyThing"   );
 
             rtr.on('route:things', function(){
                 var layout = new BbmLayout({"main": ThingsView});
@@ -17,6 +18,43 @@ define(
                 layout
                     .setup()
                     .render();
+            });
+
+            rtr.on( 'route:viewThing', function( id ){
+                var layout = new BbmLayout({"main": ThingView});
+
+                layout
+                    .setup()
+                    .render({
+                        "main": {
+                            "id": id
+                        }
+                    });
+            });
+
+            rtr.on( 'route:do', function( action ){
+                var layout = new BbmLayout({"main": ThingView});
+
+                layout
+                    .setup()
+                    .render({
+                        "main": {
+                            "action": action
+                        }
+                    });
+            });
+
+            rtr.on( 'route:modifyThing', function( action, id ){
+                var layout = new BbmLayout({"main": ThingView});
+
+                layout
+                    .setup()
+                    .render({
+                        "main": {
+                            "action": action,
+                            "id": id
+                        }
+                    });
             });
         }
 
