6005c929b0646e973f2d848afcf889f64691a15e
rockerest <websites@thomasrandolph.info>
2014-07-12 23:12:57 -0500

    Handle events regarding things

--- src/events/things.js
+++ src/events/things.js
@@ -1,10 +1,19 @@
 define(
-    ["backbone", "underscore"],
+    [
+    	// Libraries
+    	"backbone", "underscore"
+    ],
     function( Backbone, _ ){
-        var vent = window.bbm.channels.things || _.extend( {}, Backbone.Events );
+        var vent = window.bbm.channels.things || _.extend( {}, Backbone.Events ),
+        	rtr = Backbone.Router.extend(),
+        	workspace = new rtr();
 
         vent.on( "add:thing", function(){
-        	Backbone.Router.navigate( "things/new", {"trigger": true} );
+        	workspace.navigate( "thing/new", {"trigger": true} );
+        });
+
+        vent.on( "add:thing:cancel", function(){
+        	workspace.navigate( "things", {"trigger": true} );
         });
 
         window.bbm.channels.things = vent;
