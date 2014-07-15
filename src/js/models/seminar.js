037332a17aac997cdb6b577b7572d867be5f72ef
rockerest <websites@thomasrandolph.info>
2014-07-15 18:39:30 -0500

    Getters and Setters, no dependencies

--- src/js/models/seminar.js
+++ src/js/models/seminar.js
@@ -1,10 +1,63 @@
 define(
-    ["backbone"],
-    function( Backbone ){
+    [
+        // Libraries
+        "backbone", "underscore"
+    ],
+    function( Backbone, _ ){
         var SeminarModel = Backbone.Model.extend({
                 "urlRoot": "/seminar.php?p=",
                 "defaults": {
-                    "description": "Seminar 1"
+                    "name": "Seminar 1",
+                    "students": []
+                },
+
+                // GETTERS
+                "getName": function(){
+                    return this.get( "name" );
+                },
+                "getStudents": function(){
+                    return this.get( "students" );
+                },
+
+                // SETTERS
+                "setName": function( name ){
+                    this.set( "name", name );
+                },
+                "setStudents": function( students ){
+                    this.set( "students", students );
+                },
+
+                // HELPERS
+                "isStudentEnrolled": function( student ){
+                    var id = student.get( "id" );
+
+                    return _( this.getStudents() ).indexOf( id ) > -1;
+                },
+
+                "enrollStudent": function( student ){
+                    var id = student.get( "id" ),
+                        students = this.getStudents();
+
+                    if( id && !this.isStudentEnrolled( student ) ){
+                        students.push( id );
+                        this.setStudents( students );
+                        this.save();
+                    }
+                },
+
+                "removeStudent": function( student ){
+                    var id = student.get( "id" ),
+                        students = this.getStudents(),
+                        removed;
+
+                    if( id && this.isStudentEnrolled( student ) ){
+                        removed = _( students ).reject( function( s, i ){
+                            return s === id;
+                        });
+
+                        this.setStudents( removed );
+                        this.save();
+                    }
                 }
             });
 
--- src/js/models/student.js
+++ src/js/models/student.js
@@ -1,10 +1,59 @@
 define(
-    ["backbone"],
-    function( Backbone ){
+    [
+        // Libraries
+        "backbone", "underscore"
+    ],
+    function( Backbone, _ ){
         var StudentModel = Backbone.Model.extend({
                 "urlRoot": "/student.php?p=",
                 "defaults": {
-                    "description": "Joe Student"
+                    "name": "Joe Student",
+                    "seminars": []
+                },
+
+                // GETTERS
+                "getName": function(){
+                    return this.get( "name" );
+                },
+
+                "getSeminars": function(){
+                    return this.get( "seminars" );
+                },
+
+
+                // SETTERS
+                "setName": function( name ){
+                    this.set( "name", name );
+                },
+
+                "setSeminars": function( seminars ){
+                    this.set( "seminars", seminars );
+                },
+
+                // HELPERS
+                "addSeminar": function( seminar ){
+                    var id = seminar.get( "id" ),
+                        seminars = this.getSeminars();
+
+                    if( id ){
+                        seminars.push( id );
+                        this.setSeminars( seminars );
+                        this.save();
+                    }
+                },
+
+                "removeSeminar": function( seminar ){
+                    var id = seminar.get( "id" ),
+                        seminars = this.getSeminars();
+
+                    if( id ){
+                        seminars = _( seminars ).reject( function( sem ){
+                            return id == sem;
+                        });
+
+                        this.setSeminars( seminars );
+                        this.save();
+                    }
                 }
             });
 
