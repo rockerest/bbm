55dedec62093708e65a789d86c1df71f6b224d28
rockerest <websites@thomasrandolph.info>
2014-07-31 14:03:45 -0500

    "default" layout, uses inheritance

--- src/js/layouts/scheleton.js
+++ src/js/layouts/scheleton.js
@@ -1,30 +1,29 @@
 define(
-	["backbone", "underscore", "layout", "text!templates/layouts/scheleton.html", "views/chrome/navigation"],
-	function( Backbone, _, Layout, LytTemplate, NavView ){
-		var ScheletonLayout = function( regions ){
-			this.regions = {
+	[
+		// Libraries
+		"backbone", "underscore",
+		// Helpers
+		"utilities",
+		// Dependencies
+		"bases/layout", "text!templates/layouts/scheleton.html", "views/chrome/navigation"],
+	function( Backbone, _, Utilities, Layout, LytTemplate, NavView ){
+		var ScheletonLayout = function( data ){
+			Layout.call( this, data );
+
+			_.extend( this.regions, {
 				"navigation":	"nav",
 				"main":			"#application"
-			};
+			});
 
-			this.parts = {
+			_.extend( this.parts, {
 				"navigation":	NavView
-			};
+			});
 
-			if( regions && regions instanceof Object ){
-				this.assignParts( regions );
-			}
+			this.output = "body";
+			this.template = _.template( LytTemplate );
 		};
 
-		ScheletonLayout.prototype = new Layout();
-		ScheletonLayout.prototype.constructor = ScheletonLayout;
-
-		ScheletonLayout.prototype.setup = function(){
-			this.output		= "body";
-			this.template	= _.template( LytTemplate );
-
-			return this;
-		};
+		Utilities.extend( Layout, ScheletonLayout );
 
 		return ScheletonLayout;
 	}
