b8ae3d07a069e13251f251014dffcf37f6970e1c
rockerest <websites@thomasrandolph.info>
2014-09-29 18:02:21 -0500

    Simpler Layouts

--- src/js/bases/layout.js
+++ src/js/bases/layout.js
@@ -1,86 +1,32 @@
 define(
-    ["backbone", "underscore"],
-    function( Backbone, _ ){
-        var Layout = function( parts ){
+    ["backbone", "underscore", "bases/region"],
+    function( Backbone, _, Region ){
+        var Layout = function( options ){
+            this.options = options;
             this.regions = {};
-            this.parts = {};
-            this.output = 'body';
+        };
 
-            if( parts && parts instanceof Object ){
-                var self = this;
+        Layout.prototype.render = function(){
+            var self = this,
+                view = Backbone.View.extend({
+                    "el": self.options.el,
+                    "template": _.template( self.options.template ),
 
-                _( parts ).each( function( part, name, i ){
-                    self.parts[ name ] = part;
+                    "render": function(){
+                        this.$el.html( this.template() );
+                        _( self.options.regions ).each( function( selector, name ){
+                            self.regions[ name ] = new Region( selector );
+                        });
+
+                        return this;
+                    },
+
+                    "initialize": function(){
+                        this.render();
+                    }
                 });
-            }
-        };
 
-        Layout.prototype.build = function( data, optionalOutputSelector ){
-            var def = {
-                    "render": {},
-                    "construct": {},
-                    "passdown": {}
-                };
-
-            // If an output selector has been provided, use that as the output location
-            if( optionalOutputSelector ){
-                this.output = optionalOutputSelector;
-            }
-
-            data = _.extend( {}, def, data );
-
-            var self = this;
-
-            return Backbone.View.extend({
-                "el": self.output,
-                "template": self.template,
-
-                "render": function(){
-                    var isLayout, current, construct, passdown, el;
-
-                    // If any data should be rendered directly into this layout-view, do that here
-                    this.$el.html( this.template( data.render ) );
-
-                    // for each part specified in the list of parts
-                    _( self.parts ).each( function( part, name ){
-                        // Set the element to the selector present in the list of regions
-                        el = {
-                            "el": self.regions[ name ]
-                        };
-
-                        // determine whether this part is a layout
-                        isLayout = part.prototype instanceof Layout;
-
-                        // collect the information to pass into the constructor
-                        construct = data.construct[ name ] ? data.construct[ name ] : {};
-                        // collect the information to pass through to the render function
-                        passdown = data.passdown[ name ] ? data.passdown[ name ] : {};
-
-                        // If this part is not a layout, merge the object containing the correct element with the data for the constructor
-                        if( !isLayout ){
-                            construct = _.extend( {}, el, construct );
-                        }
-
-                        // Set the "current" object to a new-ed instance of the part after passing in the construction data
-                        current = new part( construct );
-
-                        // if this current part IS a layout, call render on it and pass in the passthrough data
-                        if( isLayout ){
-                            current.render( passdown, el.el );
-                        }
-                    });
-
-                    return this;
-                },
-
-                "initialize": function(){
-                    this.render();
-                }
-            });
-        };
-
-        Layout.prototype.render = function( data, optionalOutputSelector ){
-            return new (this.build( data, optionalOutputSelector ))();
+            return new view();
         };
 
         return Layout;
