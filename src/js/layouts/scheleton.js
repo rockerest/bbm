b3048dc8a7c58b123f3628f5a2ae8716d09d803a
rockerest <websites@thomasrandolph.info>
2014-09-29 18:03:57 -0500

    "Simpler" layout construction?

--- src/js/layouts/scheleton.js
+++ src/js/layouts/scheleton.js
@@ -5,21 +5,31 @@ define(
 		// Helpers
 		"utilities",
 		// Dependencies
-		"bases/layout", "text!lyt/scheleton.html", "views/chrome/navigation"],
-	function( Backbone, _, Utilities, Layout, LytTemplate, NavView ){
-		var ScheletonLayout = function( data ){
-			Layout.call( this, data );
+		"bases/layout", "bases/region", "text!lyt/scheleton.html", "views/chrome/navigation"],
+	function( Backbone, _, Utilities, Layout, Region, tmpl, NavView ){
+		var ScheletonLayout = function( options ){
+			var settings = {
+					"regions": {}
+				},
+				defaults = {
+					"regions": {
+						"navigation":	"nav",
+						"content":		"#main",
+						"footer":		"footer"
+					},
+					"template": tmpl,
+					"el": "body"
+				};
 
-			_.extend( this.regions, {
-				"navigation":	"nav",
-				"main":			"#main"
-			});
+			settings = Utilities.prepareSettings( settings, defaults, options );
 
-			_.extend( this.parts, {
-				"navigation":	NavView
-			});
+			Layout.call( this, settings );
+		};
 
-			this.template = _.template( LytTemplate );
+		ScheletonLayout.prototype.render = function(){
+			Layout.prototype.render.call( this );
+
+			this.regions.navigation.show( NavView );
 		};
 
 		Utilities.extend( Layout, ScheletonLayout );
